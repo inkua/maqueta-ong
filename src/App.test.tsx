959675c4aff90cc54926c9d6077f607393cf6b41
Leonel B. Bravo <leobravolbb2002bej@gmail.com>
2023-04-07 13:48:09 -0300

    Refactoring dir

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Site on construction/i);
   expect(linkElement).toBeInTheDocument();
 });
