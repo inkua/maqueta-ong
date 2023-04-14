57cbf31de185991452723c13c1a654dca884861a
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2023-04-14 16:19:01 -0300

    redundant tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,5 @@
+/*
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
@@ -7,3 +9,5 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/Site on construction/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+*/
