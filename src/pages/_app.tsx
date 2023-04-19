09009edffa6a3706a4bd8cb00e1b6bfa6e90bb4d
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2023-04-19 16:47:46 +0000

    update dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Layout from 'components/Layout/Layout'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Layout from 'components/Layout/Layout';
+import Header from 'components/Header/Header';
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Head>
-        <title>Maqueta ONG</title>
-      </Head>
+    <Head>
+      <title>Maqueta ONG</title>
+    </Head>
+      <Header></Header>
       <Component {...pageProps} />
     </Layout>
   )
