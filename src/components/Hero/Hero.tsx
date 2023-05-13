b5c4397dbee7845cfb9f9efc68a139a50c5a0b3b
BaironCano <baironvin13616@gmail.com>
2023-05-12 20:11:07 -0500

    feat: hero component ready.

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,62 @@
+import Image, { StaticImageData } from 'next/image';
+import { LegacyRef, useEffect, useRef, useState } from 'react';
+import styles from './hero.module.css';
+
+type PropsHero = {
+  data: {
+    title: string;
+    description: string;
+    buttonContent: string;
+    fillButton?: boolean;
+    urlImage: string | StaticImageData;
+    urlVideo?: string;
+  }
+}
+
+const Hero = ({ data }: PropsHero) => {
+  const [videoPlaying, setVideoPlaying] = useState<boolean>(false);
+
+  const {
+    title,
+    description,
+    buttonContent,
+    fillButton = false,
+    urlImage,
+    urlVideo
+  } = data;
+
+  return (
+    urlVideo ?
+      <iframe
+        width="100%"
+        height="100%"
+        src={urlVideo}
+        title={title}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        className={styles.hero__video}
+        allowFullScreen
+        >
+      </iframe>
+      :
+    <section className={styles.hero}>
+      <Image src={urlImage} alt={title} className={styles.hero__img} priority />
+      <div className={`${styles.hero__content}`}>
+        <h2 className={styles.hero__title}>{title}</h2>
+        <p className={styles.hero__description}>{description}</p>
+        <button
+          type='button'
+          className={`${styles.hero__button} button-transition
+                      ${fillButton ? styles.button__fill : styles.button__border}`}>
+            {buttonContent}
+        </button>
+      </div>
+      <div className={styles.buttons__lang}>
+        <button type='button' className={styles['hero__button-en']}>EN</button>
+        <button type='button' className={styles['hero__button-ykp']}>YKP</button>
+      </div>
+      <div className={styles.hero__degradient}></div>
+    </section>
+  )
+}
+
+export default Hero
