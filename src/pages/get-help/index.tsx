9a4015b618ca6d80b4539532c9f8bd320ea7cd0b
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2023-05-24 01:44:58 +0000

    [FIX] Harcoded data

--- src/pages/get-help/index.tsx
+++ src/pages/get-help/index.tsx
@@ -1,18 +1,9 @@
 import Accordion from "@Component/Accordion"
+import DATA from 'data/ong'
 
 const GetHelp = () => {
   //data for tests
-  const data = [
-    {
-      title: 'ONG #1', //title that the accordion will receive
-      description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimd minim“', //description that the accordion will receive
-      isOpen: true, //an optional parameter that sets the accordion open or close, depends on value
-    },
-    {
-      title: 'ONG #2',
-      description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimd minim“'
-    }
-  ]
+  const data = DATA.getHelp.accordions
 
   return (
     <div className="container my-5">
