51409c8fcf1b4eb0ac643b99cbd519f862d040ae
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2023-05-26 16:08:38 -0300

    34 pybalt delete hardcode (#35)
    * [ADD] Mandatory key prop
    
    * [FIX] Harcoded data
    
    * [FIX] Avoiding hardcode
    
    * [FIX] Keys properties of a list of components
    
    - Delete key prop as is defined by react by default.
    - Set a different id key for each component: their names.

--- src/pages/get-help/index.tsx
+++ src/pages/get-help/index.tsx
@@ -1,23 +1,15 @@
 import Accordion from "@Component/Accordion"
+import DATA from 'data/ong'
 
 const GetHelp = () => {
   //data for tests
-  const data = [
-    {
-      title: 'ONG #1', //title that the accordion will receive
-      description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimd minim“', //description that the accordion will receive
-      isOpen: true, //an optional parameter that sets the accordion open or close, depends on value
-    },
-    {
-      title: 'ONG #2',
-      description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimd minim“'
-    }
-  ]
+  const data = DATA.getHelp.accordions
 
   return (
     <div className="container my-5">
-      <Accordion data={data[0]} />
-      <Accordion data={data[1]} />
+      {data.map((item) => (
+        <Accordion key={item.title} data={item} />
+      ))}
     </div>
   )
 }
