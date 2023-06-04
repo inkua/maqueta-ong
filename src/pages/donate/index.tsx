b4c6bf8eecb266a51e57fa22626418d1a4ffb349
Mario A <mac132@live.com.mx>
2023-06-03 22:50:20 -0600

    [Add] Styles

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -8,50 +8,55 @@ const Donate = () => {
   return (
     <>
       <Hero data={DATA.pages.donatePage.donateHero} />
-      <section>
-        <h2>Follow up our guide</h2>
+
+      <section className="container">
+        <h2 className="p-5 my-5">Follow up our guide</h2>
       </section>
 
-      <section>
-        <div>
-          <p>HOW TO DONATE</p>
-        </div>
+      <section className="container d-flex flex-column">
 
-        <div>
-          {
-            tips.map((tip) => (
-              <Accordion key={tip.title} data={tip} />
-            ))
-          }
-        </div>
-      </section>
+        <section className="w-100 d-flex flex-xl-row flex-md-row flex-column flex-sm-column justify-content-between px-5 mt-5">
+          <div className="w-50 px-5">
+            <p className="w-75 fw-bold p-2 border border-dark border-2 rounded text-center fs-4">HOW TO DONATE?</p>
+          </div>
 
-      <section>
-      <div>
-          <p>DO'S</p>
-        </div>
+          <div className="w-50 px-5">
+            {
+              tips.map((tip) => (
+                <Accordion key={tip.title} data={tip} />
+              ))
+            }
+          </div>
+        </section>
 
-        <div>
-          {
-            tips.map((tip) => (
-              <Accordion key={tip.title} data={tip} />
-            ))
-          }
-        </div>
-      </section>
+        <section className="w-100 d-flex flex-row justify-content-between px-5 my-5 py-5">
+        <div className="w-50 px-5">
+            <p className="w-75 fw-bold p-2 border border-dark border-2 rounded text-center fs-4">DO'S</p>
+          </div>
 
-      <section>
-      <div>
-          <p>DON'T</p>
-        </div>
+          <div className="w-50 px-5">
+            {
+              tips.map((tip) => (
+                <Accordion key={tip.title} data={tip} />
+              ))
+            }
+          </div>
+        </section>
 
-        <div>
-          {
-            tips.map((tip) => (
-              <Accordion key={tip.title} data={tip} />
-            ))
-          }
-        </div>
+        <section className="w-100 d-flex flex-row justify-content-between px-5 mb-5 pb-5">
+        <div className="w-50 px-5">
+            <p className="w-75 fw-bold p-2 border border-dark border-2 rounded text-center fs-4">DON'T</p>
+          </div>
+
+          <div className="w-50 px-5">
+            {
+              tips.map((tip) => (
+                <Accordion key={tip.title} data={tip} />
+              ))
+            }
+          </div>
+        </section>
+
       </section>
     </>
   )
