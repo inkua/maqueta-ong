6d86cc861794b74b3894e9bac9f692b8c33e6f3e
Leonel B. Bravo <96897286+pybalt@users.noreply.github.com>
2023-06-11 20:15:14 -0300

    [FIX] Linting [RENAME] Properties to english

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,20 @@
-const Card = ({ titulo, texto }) => {
+import PropTypes from "prop-types";
+
+const Card = ({ title, text }) => {
   return (
     <div className="card bg-secondary border border-0 text-white">
       <div className="card-body" id='card-body'>
-        <h2 className="card-title text-center py-3">{titulo}</h2>
-        <p className="card-text py-4 text-start p-5">{texto}</p>
+        <h2 className="card-title text-center py-3">{title}</h2>
+        <p className="card-text py-4 text-start p-5">{text}</p>
       </div>
     </div>
   );
 };
+
+Card.prototype.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
 
 export default Card;
