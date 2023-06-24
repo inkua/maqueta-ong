cc3f9997574ac6e6c69627b3467e0d47a02a1a56
BaironCano <baironvin13616@gmail.com>
2023-06-24 16:00:00 -0500

    [FIX] transition and height of the accordion component corrected.

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import styles from './accordion.module.css'
 
 type PropsAccordion = {
@@ -11,18 +11,32 @@ type PropsAccordion = {
 
 const Accordion = ({ data: { title, description, isOpen = false } }: PropsAccordion) => {
   const [open, setOpen] = useState(isOpen);
-  const descriptionRef = useRef(null);
+
+  const toggleAccordion = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className={styles.accordion}>
-      <h3 className={`${styles.accordion__title} ${open && styles['accordion__title-active']}`} onClick={() => setOpen(!open)}>{title}</h3>
-      <p
-        className={`${styles.accordion__description} ${open && styles['accordion__description-active']}`}
-        ref={descriptionRef}>
-          {open && description}
-      </p>
+      <h3
+        className={`${styles.accordion__title} ${open && styles['accordion__title-active']}`}
+        onClick={toggleAccordion}
+      >
+          {title}
+      </h3>
+      <div
+        className={styles.content__description}
+        style={{
+          maxHeight: open ? '300px' : '0',
+          opacity: open ? '1' : '0',
+        }}
+      >
+        <p className={styles.description}>
+          {description}
+        </p>
+      </div>
     </div>
   )
 }
 
-export default Accordion
+export default Accordion;
