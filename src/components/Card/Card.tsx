ace8d1f506a32f31b89516d3b6a7f1a9aef95871
BaironCano <baironvin13616@gmail.com>
2023-08-08 15:18:31 -0500

    [UPDATE] maximum width of the card component can be provided

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,24 @@
+import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import styles from "./Card.module.css";
 
 type CardProps = {
   title: string;
   description: string;
+  widthCard?: string;
 };
 
-const Card = ({ title, description }: CardProps) => {
+const Card = ({ title, description, widthCard = "500px" }: CardProps) => {
+  const articleRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const articleCurrent = articleRef.current;
+    articleCurrent.style.maxWidth = widthCard;
+  }, [widthCard])
+
+
   return (
-    <article className={styles.card}>
+    <article ref={articleRef} className={styles.card}>
       <h3 className={styles.card__title}>{title}</h3>
       <p className={styles.card__description}>{description}</p>
     </article>
