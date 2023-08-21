a79517ba6cd043a5a3ae2bbfceec3be716c9a520
Bairon Cano <84099271+bairon-cano@users.noreply.github.com>
2023-08-20 20:06:05 -0500

    Volunteer page (#63)
    * [ADD] ong.ts file changed to ong.json, also the data context and the useContextData were created
    
    * [UPDATE] imports of the data, updated on all pages, consuming the context
    
    * [FIX] bugs in the images of some components, fixed, they were also moved
    
    * [UPDATE] maximum width of the card component can be provided
    
    * [ADD] volunteer page ready

--- src/pages/volunteer/index.tsx
+++ src/pages/volunteer/index.tsx
@@ -1,7 +1,8 @@
+import useContextData from 'hooks/useContextData';
 import Card from '@Component/Card/Card';
 import Hero from '@Component/Hero';
+import BtnBeAVolunteer from '@Component/Button';
 import styles from './volunteer.module.css'
-import useContextData from 'hooks/useContextData';
 
 const Volunteer = () => {
   const dataVolunteerPage = useContextData().pages.volunteer;
@@ -17,11 +18,11 @@ const Volunteer = () => {
       <Hero data={dataHero} />
       <section className='sections-container'>
         <article className='article'>
-          <h2 className='article-title'>{dataVolunteerDoes.title}</h2>
+          <h2 className={styles.align__title}>{dataVolunteerDoes.title}</h2>
           <p className={`article-description ${styles.align__paragraph}`}>
             {dataVolunteerDoes.description}
           </p>
-          <section className={`article-section ${styles.section__history}`}>
+          <section className={`article-section ${styles.objectives__requirements}`}>
             <Card
               key={dataObjectivesAndRequirements.objetives.title}
               title={dataObjectivesAndRequirements.objetives.title}
@@ -34,6 +35,22 @@ const Volunteer = () => {
             />
           </section>
         </article>
+        <div>
+          <h2 className={styles.align__title}>{dataExperiencesOfVolunteers.title}</h2>
+          <article className={`article-section ${styles.experiences}`}>
+            {
+              dataExperiencesOfVolunteers.experiences.map((experience) => (
+                <Card
+                  key={experience.title}
+                  title={experience.title}
+                  description={experience.description}
+                  widthCard='320px'
+                />
+              ))
+            }
+          </article>
+        </div>
+        <BtnBeAVolunteer title='Be a volunteer' url='/volunteer-form' typeArrow={'\u{1F86A}'} />
       </section>
     </>
   )
